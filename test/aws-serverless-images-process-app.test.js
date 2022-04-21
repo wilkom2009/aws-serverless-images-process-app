f2d190113e42ba9abb9e89abac424e312a19d6bc
Wilson KOMLAN <koffikomlan01@gmail.com>
2022-04-21 11:34:09 +0000

    Initial commit

---
+++ test/aws-serverless-images-process-app.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsServerlessImagesProcessApp = require('../lib/aws-serverless-images-process-app-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-images-process-app-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsServerlessImagesProcessApp.AwsServerlessImagesProcessAppStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
